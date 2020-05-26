e9ec05238bc5ac6b1dae4fc951bbb4a424ab8dc1
fpacaud <francoispacaud8@gmail.com>
2020-05-26 21:05:37 +0200

    update presentation

--- cours/ddi/model.py
+++ cours/ddi/model.py
@@ -3,27 +3,61 @@ from numpy.random import binomial
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-def discrete_simulation(T, n, β, γ, s0=999, i0=1, r0=0):
+def discrete_simulation(T, n, beta, gamma, s0=999, i0=1, r0=0):
     states = np.zeros((T, 3), dtype=np.int)
     states[0, :] = [s0, i0, r0]
     for t in range(T-1):
         s, i, r = states[t, :]
-        contagions = binomial(s, β / n * i)
-        removals = binomial(i, γ)
+        contagions = binomial(s, beta / n * i)
+        removals = binomial(i, gamma)
         states[t+1, 0] = s - contagions
         states[t+1, 1] = i + contagions - removals
         states[t+1, 2] = r + removals
     return states
 
-states_1 = discrete_simulation(800, 1000, 0.1, 0.01)
-states_2 = discrete_simulation(800, 1000, 0.1, 0.05)
-states_3 = discrete_simulation(800, 1000, 0.01, 0.01)
-states_4 = discrete_simulation(800, 1000, 0.02, 0.01)
+beta = 0.1
+gamma = 0.01
+states_1 = discrete_simulation(800, 1000, beta, gamma)
 
-nsimu = 100
-for i in range(nsimu):
-    states = discrete_simulation(800, 1000, 0.1, 0.01)
-    plt.plot(states, lw=.2)
+fig, ax = plt.subplots(figsize=(9, 6))
+ax.plot(states_1[:, 0], lw=2.0, label="S")
+ax.plot(states_1[:, 1], lw=2.0, label="I")
+ax.plot(states_1[:, 2], lw=2.0, label="R")
+ax.legend()
+ax.set_ylabel("Number of cases")
+ax.set_xlabel("Timesteps")
+ax.set_title(f"Epidemic, beta={beta} gamma={gamma}")
+fig.savefig("simu_1.pdf")
+
+
+def run_simulation(nsimu, beta=0.1, gamma=0.01, outname="simu.pdf"):
+    fig, ax = plt.subplots(figsize=(9, 6))
+    ax.set_ylabel("Number of cases")
+    ax.set_xlabel("Timesteps")
+    ax.set_title(f"Epidemic, beta={beta} gamma={gamma}")
+    for i in range(nsimu):
+        states = discrete_simulation(800, 1000, beta, gamma)
+        ax.plot(states[:, 0], lw=.2, c="darkblue")
+        ax.plot(states[:, 1], lw=.2, c="darkred")
+        ax.plot(states[:, 2], lw=.2, c="darkgreen")
+    fig.savefig(outname)
+    fig.clear()
+
+nsimu = 10
+run_simulation(nsimu, beta=beta, gamma=gamma, outname="simu_2.pdf")
+
+beta = 0.01
+gamma = 0.01
+run_simulation(nsimu, beta=beta, gamma=gamma, outname="simu_3.pdf")
+
+beta = 0.02
+gamma = 0.01
+run_simulation(nsimu, beta=beta, gamma=gamma, outname="simu_4.pdf")
+
+beta = 0.1
+gamma = 0.05
+run_simulation(nsimu, beta=beta, gamma=gamma, outname="simu_5.pdf")
+
 
 def continuous_simulation(T, n, beta, gamma):
     def sir(y, t):
@@ -36,4 +70,16 @@ def continuous_simulation(T, n, beta, gamma):
     sol = odeint(sir, y0, t)
     return sol
 
-sol = continuous_simulation(800, 1000, 0.1, 0.01)
+beta = 0.1
+gamma = 0.01
+sol = continuous_simulation(800, 1000, beta, gamma)
+fig, ax = plt.subplots(figsize=(9, 6))
+ax.plot(sol[:, 0], lw=2.0, label="S")
+ax.plot(sol[:, 1], lw=2.0, label="I")
+ax.plot(sol[:, 2], lw=2.0, label="R")
+ax.legend()
+ax.set_ylabel("Number of cases")
+ax.set_xlabel("Timesteps")
+ax.set_title(f"Epidemic, beta={beta} gamma={gamma}")
+fig.savefig("simu_6.pdf")
+
